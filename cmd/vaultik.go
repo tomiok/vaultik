7e8b53066e9377ff71d551b8f03557d4ebca4f97
tomas lingotti <tomaslingotti@gmail.com>
2020-11-23 23:17:08 -0300

    WIP

--- cmd/vaultik.go
+++ cmd/vaultik.go
@@ -1,8 +1,8 @@
 package cmd
 
 type vaultik struct {
-	encodingKey string
-	filename    string
+	encodingKey string // is the key to encrypt the entry (the value given).
+	filename    string // where the entry is going to be stored.
 }
 
 func newVaultik(encodingKey, filename string) vaultik {
@@ -12,13 +12,13 @@ func newVaultik(encodingKey, filename string) vaultik {
 	}
 }
 
-//setValue key is the actual key to identify the entry
+//setValue key is the actual key to identify the entry.
 func (v *vaultik) setValue(key, value string) error {
-
+	return nil
 }
 
 //getValue key is the actual key to identify the entry, return the encrypted/encoded value and an error (nil if any
-// problem occurred)
+// problem occurred).
 func (v *vaultik) getValue(key string) (string, error) {
-
+	return "", nil
 }
