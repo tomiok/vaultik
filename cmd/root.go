bfc27f25470eed29e099b90548e0c5d922fa225d
Tomas Lingotti <tomaslingotti@gmail.com>
2021-12-11 22:59:47 -0300

    cleaning up the mess

--- cmd/root.go
+++ cmd/root.go
@@ -4,23 +4,19 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
-
-	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var (
 	filename    string
 	encodingKey string
-	cfgFile     string
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:                        "vaultik",
-	Short:                      "A brief description of your application",
-	Long:                       `this is VAULTIK`,
-	Example:                    "",
+	Use:     "vaultik",
+	Short:   "A brief description of your application",
+	Long:    `this is VAULTIK, a CLI interface for add variables in a secure way`,
+	Example: "",
 }
 
 func Execute() {
@@ -40,33 +36,5 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVarP(&filename, "file", "f", "", "File vault")
-	rootCmd.PersistentFlags().StringVarP(&encodingKey, "encodingKey", "k", "", "Encoding encodingKey")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".vaultik")
-	}
-
-	// read in environment variables that match
-	viper.AutomaticEnv()
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	rootCmd.PersistentFlags().StringVarP(&encodingKey, "encodingKey", "k", "", "Encoding key")
 }
