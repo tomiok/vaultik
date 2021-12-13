ac7c783f85a28d4dc21e18e03b2aa2466b2d8db0
Tomas Lingotti <tomaslingotti@gmail.com>
2021-12-13 11:52:46 -0300

    cleanup get commmand

--- cmd/get.go
+++ cmd/get.go
@@ -6,9 +6,9 @@ import (
 )
 
 var getCmd = &cobra.Command{
-	Use:   "get [-d | --decrypt] {key} | will return the value of the given key. Use -d for read the value in plain text",
+	Use:   "get [-d | --decrypt (true|false)] {key} | will return the value of the given key. Use -d for read the value in plain text",
 	Short: "",
-	Long:  `Get the value, the cipher encodingKey is needed (Long)`,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		vault := getVaultikData()
 		if len(args) == 0 {
@@ -24,8 +24,7 @@ var getCmd = &cobra.Command{
 			fmt.Println(fmt.Sprintf("error: %v, please try again", err))
 			return
 		}
-
-		fmt.Println("result: " + res)
+		fmt.Println(res)
 	},
 }
 
