7ee0752fcf4a5534a50e24855bc42677b8c675d6
Tomas Lingotti <tomaslingotti@gmail.com>
2021-12-20 03:13:02 -0300

    add delete command logic

--- cmd/delete.go
+++ cmd/delete.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
 )
 
 var delCmd = &cobra.Command{
@@ -11,8 +13,28 @@ var delCmd = &cobra.Command{
 	Long:  `delete a file given a key in plain text`,
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deleting...")
+		key := args[0]
+		err := deleteFile(key)
+
+		if err != nil {
+			fmt.Println(fmt.Sprintf("cannot delete file %s, due to error %s", key, err.Error()))
+			return
+		}
+
+		fmt.Println(fmt.Sprintf("file %s deleted", key))
 	},
+}
+
+func deleteFile(key string) error {
+	home, err := os.UserHomeDir()
+
+	if err != nil {
+		return err
+	}
+
+	p := filepath.Join(home, dirSecure, key)
+
+	return os.Remove(p)
 }
 
 func init() {
