c524d99616cbcb20f4ef0c0dc0a2a94316310f6e
Tomas Lingotti <tomaslingotti@gmail.com>
2021-12-25 19:05:01 -0300

    add method flag

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -9,7 +9,7 @@ import (
 var method string
 
 var migrateCmd = &cobra.Command{
-	Use:     "migrate {user} {path to id_rsa} {destination}",
+	Use:     "migrate {user} {path to id_rsa} {destination} [-m=privateKey]",
 	Short:   "migrate should send in scp protocol all the secured and hashed pair of key-values",
 	Long:    ``,
 	Example: "migrate tomas p45w0rd1 example.com:22",
@@ -17,22 +17,42 @@ var migrateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		username := args[0]
-		pass := args[1]
+		access := args[1]
 		host := args[3]
 
-		sshClientConfig := withPassword(username, pass, nil)
+		if method == "" {
+			sshClientConfig := withPassword(username, access, nil)
 
-		client := NewClient(host, &sshClientConfig)
+			client := NewClient(host, &sshClientConfig)
 
-		err := client.Connect()
+			err := client.Connect()
 
-		if err != nil {
-			fmt.Println(err.Error())
-			return
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+		} else {
+			sshClientConfig, err := withPrivateKey(username, access, nil)
+			if err != nil {
+				fmt.Println(fmt.Sprintf("cannot access to remote address %s", err.Error()))
+				return
+			}
+
+			client := NewClient(host, &sshClientConfig)
+
+			err = client.Connect()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
 		}
 	},
 }
 
 func init() {
+	migrateCmd.Flags().StringVarP(&method, "method", "m", "", "method is the auth method for" +
+		"ths SSH connection. use privateKey and provide the PATH to your private key, or leave it blank to use " +
+		"standard user + password connection")
 	rootCmd.AddCommand(migrateCmd)
 }
